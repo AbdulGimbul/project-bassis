93277b137dae1ec3a9f26d5cf00b1535c5c23d21
AbdulGimbul <azizgimboell@gmail.com>
2021-07-27 11:08:47 +0700

    layouting login and register

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -38,7 +38,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Jumlah Transaksi Nasabah</div>
+                                Jumlah Pemasukan Bulan Ini</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 4.100.000</div>
                         </div>
                         <div class="col-auto">
@@ -55,7 +55,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Sampah Terkumpul
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
@@ -83,7 +83,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
+                                Jenis-jenis Sampah</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                         </div>
                         <div class="col-auto">
@@ -104,7 +104,7 @@
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik Penjualan Sampah</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Grafik Pemasukan Bulan Ini</h6>
                     <div class="dropdown no-arrow">
                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
